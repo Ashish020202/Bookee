065ee506c889a53c3ff1cb3729e096d0cce9ff94
Ashish kumar <ashishgupta020202@gmail.com>
2024-08-17 12:24:21 +0530

    react hook form

--- bookee/frontend/src/components/Signup.jsx
+++ bookee/frontend/src/components/Signup.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Login from './login';
-// import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm } from "react-hook-form"
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
+    
     <div>
       <div className='flex justify-center m-36'>
         <div className="modal-box m-auto">
           <form method="dialog"  onSubmit={handleSubmit(onSubmit)}>
             <Link to={"/"} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
-          </form>
           <h3 className="font-bold text-lg">Sign Up</h3>
-          <form>
             <div className="py-4">
               <label className="block text-sm font-medium text-gray-700">Name</label>
               <input 
@@ -20,7 +25,9 @@ function SignUp() {
                 placeholder="Enter your name" 
                 className="input input-bordered w-full mt-1"
                 required
+                {...register("text", { required: true })}
               />
+              {errors.text && <span className='text-red-600'>This field is required</span>}
             </div>
             <div className="py-4">
               <label className="block text-sm font-medium text-gray-700">Email</label>
@@ -29,7 +36,9 @@ function SignUp() {
                 placeholder="Enter your email" 
                 className="input input-bordered w-full mt-1"
                 required
+                {...register("email", { required: true })}
               />
+              {errors.email && <span className='text-red-600'>This field is required</span>}
             </div>
             <div className="py-4">
               <label className="block text-sm font-medium text-gray-700">Password</label>
@@ -38,7 +47,9 @@ function SignUp() {
                 placeholder="Create a password" 
                 className="input input-bordered w-full mt-1"
                 required
+                {...register("password", { required: true })}
               />
+              {errors.password && <span className='text-red-600'>This field is required</span>}
             </div>
             <div className="modal-action">
               <button type="submit" className="btn btn-success w-72">Sign Up</button>
--- bookee/frontend/src/components/login.jsx
+++ bookee/frontend/src/components/login.jsx
@@ -9,16 +9,16 @@ function Login() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data)
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog" form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
             <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</Link>
-          </form>
+         
           <h3 className="font-bold text-lg">Login</h3>
-          <form>
+       
             <div className="py-4">
               <label className="block text-sm font-medium text-gray-700">Email</label>
               <input 
@@ -27,6 +27,8 @@ function Login() {
                 className="input input-bordered w-full mt-1"
                 {...register("email", { required: true })}
               />
+               {errors.email && <span className='text-red-600'>This field is required</span>}
+
             </div>
             <div className="py-4">
               <label className="block text-sm font-medium text-gray-700">Password</label>
@@ -36,16 +38,19 @@ function Login() {
                 className="input input-bordered w-full mt-1"
                 {...register("password", { required: true })}
               />
+               {errors.password && <span className='text-red-600'>This field is required</span>}
+
             </div>
             <div className="modal-action">
               <button type="submit" className="btn btn-primary w-full">Login</button>
             </div>
-          </form>
+
           <div className="text-center mt-4">
             <p className="text-lime-800">Not registered? 
-              <a href="/signup" className="text-blue-700 ml-1">Sign up here</a>
+              <Link to="/signup" className="text-blue-700 ml-1">Sign up here</Link>
             </p>
           </div>
+          </form>
         </div>
       </dialog>
     </div>
